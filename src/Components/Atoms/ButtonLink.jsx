ee01a72f619d6b69cda325d7e3e1d69603a2db6a
Yash <yashshukla336@gmail.com>
2024-03-14 17:53:14 +0530

    build: section1 fixed left side box and added new links

--- src/Components/Atoms/ButtonLink.jsx
+++ src/Components/Atoms/ButtonLink.jsx
@@ -1,9 +1,37 @@
 import { styled } from "styled-components";
+import { IoArrowForwardSharp } from "react-icons/io5";
 
 const ButtonLink = ({ title }) => {
-  return <Buttonbox>{title}</Buttonbox>;
+  return (
+    <Buttonbox>
+      {title}
+      <IoArrowForwardSharp className="icon" />
+    </Buttonbox>
+  );
 };
 
 export default ButtonLink;
+const Buttonbox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 23px;
+  font-weight: 600;
+  color: #ff0066;
+  position: relative;
+  transition: color 0.2s ease;
+  cursor: pointer;
 
-const Buttonbox = styled.div``;
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:hover .icon {
+    transform: translateX(10px);
+  }
+
+  .icon {
+    color: #ff0066;
+    transition: transform 0.2s ease;
+  }
+`;
--- src/Components/Organisms/Section1.jsx
+++ src/Components/Organisms/Section1.jsx
@@ -1,14 +1,32 @@
 import { styled } from "styled-components";
+import ButtonLink from "../Atoms/ButtonLink";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const Section1 = () => {
   return (
     <Main>
       <Left>
-        <img src="/innovation-illustration.png" alt="" className="sec1img" />
+        <Smallbox>
+          <img src="/innovation-illustration.png" alt="" className="sec1img" />
+          <AllLinks>
+            <a href="https://www.thermoking.com/na/en.html" className="alink">
+              Thermoking.com
+            </a>
+
+            <a href="https://www.trane.com/index.html" className="alink">
+              Trane.com
+            </a>
+            <Stockshow>
+              TT 285.93-1.78
+              <FaArrowDownLong className="icon" />
+            </Stockshow>
+          </AllLinks>
+        </Smallbox>
         <p className="text">
           Today, big ideas and <br /> bold action. <br />
           Tomorrow, a better <br /> world for everyone.
         </p>
+        <ButtonLink title={"See our impact"} />
       </Left>
 
       <Right>
@@ -53,4 +71,39 @@ const Left = styled.div`
 const Right = styled.div`
   object-fit: cover;
   height: 100%;
+`;
+
+const Smallbox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .alink {
+    text-decoration: none;
+    color: black;
+    font-size: 13px;
+    font-weight: 600;
+  }
+`;
+
+const AllLinks = styled.div`
+  display: flex;
+  height: 100%;
+  /* align-items: center; */
+  gap: 15px;
+  cursor: pointer;
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Stockshow = styled.div`
+  display: flex;
+  gap: 5px;
+
+  font-size: 13px;
+  font-weight: 600;
+  .icon {
+    color: #ff0066;
+    font-size: 12px;
+  }
 `;
